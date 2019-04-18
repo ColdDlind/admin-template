00c6151c7c2b7837ff80870a4d2d6f59fb92af15
dengjian <dengjian@qxy37.com>
2019-04-18 10:44:09 +0800

    init 0.0.1

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header/>
+    <SlideMenu/>
+    <router-view></router-view>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SlideMenu from "./components/SlideMenu";
+
+export default {
+  components: {
+    Header,
+    Footer,
+    SlideMenu
+  }
+};
+</script>
+
+<style>
+</style>
